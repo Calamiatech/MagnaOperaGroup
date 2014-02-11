cf042ed31fb120459c638495dcfba8e06d1181b5
Galatan Ovidiu <galatanovidiu@gmail.com>
2014-02-11 23:13:02 +0200

    Menu Fixes

--- themes/MagnaOpera/footer.php
+++ themes/MagnaOpera/footer.php
@@ -1,111 +1,102 @@
 
 <!-- Close Mainbody and start footer
-  ================================================== -->
+================================================== -->
 
 <!-- Theme Hook -->
 <?php wp_footer(); ?>
 </div>
 <!-- Close Site Container
-  ================================================== -->
-  <?php echo of_get_option('of_google_analytics'); ?>
-  
+================================================== -->
+<?php echo of_get_option('of_google_analytics'); ?>
+
 <!-- basic javascripts -->
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . "/js/main.js"; ?>"></script>
 <!-- MOBILE NAV -->
 
-<div class="mobilenavcontainer mobile">
-   <div class="mobile-logo logo">
-        <h1> <a href="#about" class="mobile">
-            <?php include(get_stylesheet_directory() . "/images/m-logo.svg"); ?>
-            </a> 
-        </h1>
-    </div>
-</div> 
+
 <div class="mobile about-contact-page hidden" id="AboutOurWork">
-    <div class="mobilenav">
-        <div class="mobile logo"><a href="<?php echo home_url(); ?>" class="mobile">
-            <?php include(get_stylesheet_directory() . "/images/m-logo.svg"); ?>
-            </a>
-        </div>
-        <div class="closebuttonhitarea">
-	        <div class="mobile close">
-	            <a href="#close">
-	            </a>
-	        </div>
-	    </div>
-        <div class="mobile menu">
-            <ul>
-            	<li><a href="#about"><h2>About</h2></a></li>
-                <li><a href="#work"><h2>Work</h2></a></li>
-                <li><a href="mailto:info@magnaoperagroup.com"><h2>Contact</h2></a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="about" class="mobile content about">
-        <?php
-            $about_page_id = 2;
-            $about_page = get_post($about_page_id);
-            $content = $about_page->post_content;
-            $content = apply_filters('the_content', $content);
-            $content = str_replace(']]>', ']]>', $content);
-            echo $content;
-        ?>
-    </div>
-    <div id="contact" class="mobile content contact">
-        <?php
-            $about_page_id = 19;
-            $about_page = get_post($about_page_id);
-            $content = $about_page->post_content;
-            $content = apply_filters('the_content', $content);
-            $content = str_replace(']]>', ']]>', $content);
-            echo $content;
-        ?>
-    </div>
-    <div id="work" class="mobile content work">
-        <!-- Project Container -->
-        <div id="workcontainer" class="mobile projects works">
-            <?php  
-/* #Grab the global ProperPagination and WP_Query instances
-===========================================================*/
-global $pp, $wp_query, $wp;
+	<div class="mobilenav">
+		<div class="mobile logo"><a href="<?php echo home_url(); ?>" onclick="" class="mobile"><img src="<?php echo content_url(); ?>/themes/MagnaOpera/images/logo_magnaoperagroup.png" alt=""></a>
+		</div>
+		<div class="closebuttonhitarea">
+			<div class="mobile close"><a href="#close">+</a></div>
+		</div>
+		<div class="mobile menu">
+			<ul>
+				<li><a href="#" onclick="jQuery('#about').fadeIn('slow'); jQuery('#work').fadeOut('slow');">About</a></li>
+				<li><a href="#" onclick="jQuery('#work').fadeIn('slow'); jQuery('#about').fadeOut('slow');">Work</a></li>
+				<li><a href="mailto:info@magnaoperagroup.com" target="_blank">Contact</a></li>
+			</ul>
 
-$counter = 1; 
-if($projects = of_get_option('of_projects_number')) 
-    { $posts_per_page = $projects; }
-else { $posts_per_page = 8;}
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		</div>
+	</div>
+	<div id="about" class="mobile content about" style="overflow: auto; display: block;">
+		<?php
+		$about_page_id = 2;
+		$about_page = get_post($about_page_id);
+		$content = $about_page->post_content;
+		$content = apply_filters('the_content', $content);
+		$content = str_replace(']]>', ']]>', $content);
+		echo $content;
+		?>
+	</div>
+	<div id="contact" class="mobile content contact">
+		<?php
+		$about_page_id = 19;
+		$about_page = get_post($about_page_id);
+		$content = $about_page->post_content;
+		$content = apply_filters('the_content', $content);
+		$content = str_replace(']]>', ']]>', $content);
+		echo $content;
+		?>
+	</div>
+	<div id="work" class="mobile content work">
+		<!-- Project Container -->
+		<div id="workcontainer" class="mobile projects works">
+			<?php  
+			/* #Grab the global ProperPagination and WP_Query instances
+			===========================================================*/
+			global $pp, $wp_query, $wp;
 
-$wp_query = new WP_Query(array('post_type' => 'portfolio', 'posts_per_page'=> $posts_per_page, 'paged' => $paged)); // Construct the custom WP_Query instance
+			$counter = 1; 
+			if($projects = of_get_option('of_projects_number')) 
+				{ $posts_per_page = $projects; }
+			else { $posts_per_page = 8;}
+			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-/* #Loop through your posts...
-======================================================*/
-while ( $wp_query->have_posts() ) : $wp_query->the_post(); 
-    $portfoliodisplay = get_post_meta(get_the_ID(), 'ag_portfolio_page_display', true);
+			$wp_query = new WP_Query(array('post_type' => 'portfolio', 'posts_per_page'=> $posts_per_page, 'paged' => $paged)); // Construct the custom WP_Query instance
 
-    if ($portfoliodisplay !== true) :
-        $post_url = get_permalink(); //Get Permalink for post
-        $thumb1 = get_post_meta($post->ID,'_thumbnail_id',false); 
-        $thumb = wp_get_attachment_image_src($thumb1[0], 'portfoliosmall', false); 
+			/* #Loop through your posts...
+			======================================================*/
+			while ( $wp_query->have_posts() ) : $wp_query->the_post(); 
+				$portfoliodisplay = get_post_meta(get_the_ID(), 'ag_portfolio_page_display', true);
 
-?>
-            <!-- Portfolio Item -->
-            <div class="portfoliolistitem outlineglow">
-                <a href="<?php echo $post_url; ?>" data-url="<?php the_ID(); ?>">
-                    <div class="imagecontainer">
-                        <img src="<?php  echo $thumb[0]; ?>" alt="" class=""/>
-                    </div>
-                    <div class="titlecontainer">
-                        <h1><?php the_title();?></h1>
-                    </div>
-                </a>
-            </div>
-            <!-- Portfolio Item -->
-<?php
-    endif; // End if($portfoliodisplay !== true)
-endwhile; //End while ( $wp_query->have_posts() ) : $wp_query->the_post(); 
-?>
-        </div>
-    </div>
+				if ($portfoliodisplay !== true) :
+					$post_url = get_permalink(); //Get Permalink for post
+					$thumb1 = get_post_meta($post->ID,'_thumbnail_id',false); 
+					$thumb = wp_get_attachment_image_src($thumb1[0], 'portfoliosmall', false); 
+
+					?>
+					<!-- Portfolio Item -->
+					<div class="portfoliolistitem outlineglow">
+						<a href="<?php echo $post_url; ?>" data-url="<?php the_ID(); ?>">
+							<div class="imagecontainer">
+								<img src="<?php  echo $thumb[0]; ?>" alt="" class=""/>
+							</div>
+							<div class="titlecontainer">
+								<h1><?php the_title();?></h1>
+							</div>
+						</a>
+					</div>
+					<!-- Portfolio Item -->
+					<?php
+					endif; // End if($portfoliodisplay !== true)
+				endwhile; //End while ( $wp_query->have_posts() ) : $wp_query->the_post(); 
+			?>
+		</div>
+	</div>
 </div>
+<a class="static_mobile_logo" href="#" class="mobile"><img src="<?php echo content_url(); ?>/themes/MagnaOpera/images/logo_magnaoperagroup_mobile.png" alt=""></a>
+
 </body>
 </html>
