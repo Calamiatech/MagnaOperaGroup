052a1dd9b7c324c82dbad2ea7fc9055fa385cf6e
Galatan Ovidiu <galatanovidiu@gmail.com>
2014-02-13 23:36:40 +0200

    more menu fixes

--- themes/MagnaOpera/footer.php
+++ themes/MagnaOpera/footer.php
@@ -23,8 +23,8 @@
 		</div>
 		<div class="mobile menu">
 			<ul>
-				<li><a id="about_button" class="underline" href="#about_content" onclick="jQuery('#about').show(); jQuery('#about_button').addClass('underline'); jQuery('#work').hide(); jQuery('#work_button').removeClass('underline'); jQuery('html, body').animate({ scrollTop: 145 }, 500); return false;">About</a></li>
-				<li><a id="work_button" href="#work_content" onclick="jQuery('#work').show(); jQuery('#work_button').addClass('underline'); jQuery('#about').hide(); jQuery('#about_button').removeClass('underline'); jQuery('html, body').animate({ scrollTop: 145 }, 500); return false;">Work</a></li>
+				<li id="about_button" class="underline" onclick="jQuery('#about').show(); jQuery('#about_button').addClass('underline'); jQuery('#work').hide(); jQuery('#work_button').removeClass('underline'); jQuery('html, body').animate({ scrollTop: 145 }, 500);">About</li>
+				<li id="work_button" onclick="jQuery('#work').show(); jQuery('#work_button').addClass('underline'); jQuery('#about').hide(); jQuery('#about_button').removeClass('underline'); jQuery('html, body').animate({ scrollTop: 145 }, 500);">Work</li>
 				<li><a href="mailto:info@magnaoperagroup.com" target="_blank">Contact</a></li>
 			</ul>
 
